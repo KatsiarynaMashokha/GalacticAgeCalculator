ce944a7b61b25cbb0659ccf5d03cab0c363a0c19
Kate Mashokha <katyamashokha@yahoo.com>
2017-09-08 20:22:07 -0700

    show different planets age upon a submit button click

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -1,19 +1,22 @@
 import { Age } from './../js/calculator.js';
 
-$(function() {
-  $('#age-form').submit(function(event) {
+$(document).ready(function() {
+  $('#submit').click(function(event) {
     event.preventDefault();
     let currentTime = new Date().getTime();
-    let userBd = new Date($('#age').val());
+    let userBd = (new Date($('#age').val())).getTime();
     let userAge = new Age(userBd, currentTime);
-    //
-    //
-    // let mercuryAgeNew = convertToMercuryAge();
-    // let marsAge = convertToMarsAge();
-    // let jupiterAge = convertToJupiterAge();
-    //
-    // $('#mercuryAge').append(mercuryAgeNew);
 
+    let mercuryAge = userAge.convertToMercuryAge();
+    let venusAge = userAge.convertToVenusAge();
+    let marsAge = userAge.convertToMarsAge();
+    let jupiterAge = userAge.convertToJupiterAge();
+
+    $('.age-stats').show();
+    $('#mercuryAge').text(mercuryAge);
+    $('#venusAge').text(venusAge);
+    $('#marsAge').text(marsAge);
+    $('#jupiterAge').text(jupiterAge);
 
   });
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,18 +1,25 @@
 import { Age } from './../js/calculator.js';
 
-$(function() {
-  $('#age-form').submit(function(event) {
+$(document).ready(function() {
+  $('#submit').click(function(event) {
     event.preventDefault();
     let currentTime = new Date().getTime();
-    let userBd = $('#age').val();
+    let userBd = (new Date($('#age').val())).getTime();
     let userAge = new Age(userBd, currentTime);
 
+    let mercuryAge = userAge.convertToMercuryAge();
+    let venusAge = userAge.convertToVenusAge();
+    let marsAge = userAge.convertToMarsAge();
+    let jupiterAge = userAge.convertToJupiterAge();
 
-    let mercuryAgeNew = convertToMercuryAge();
-    let marsAge = convertToMarsAge();
-    let jupiterAge = convertToJupiterAge();
 
-    $('#mercuryAge').append(mercuryAgeNew);
+    $('.age-stats').show();
+    $('#mercuryAge').text(mercuryAge);
+    $('#venusAge').text(venusAge);
+    $('#marsAge').text(marsAge);
+    $('#jupiterAge').text(jupiterAge);
+
+
 
 
   });
